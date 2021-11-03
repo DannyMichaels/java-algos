72c18db3d2e46fdfa0251e131adf25adb7cfb569
Daniel <itzdanielmichael@gmail.com>
2021-11-03 18:38:52 -0400

    push

--- src/App.java
+++ src/App.java
@@ -40,7 +40,6 @@ class StringAlgos {
 
 class ArrayAlgos {
   public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-    // Write your code here.
 		int i = 0;
 		int j = array.size() - 1;
 		while (i < j) {
